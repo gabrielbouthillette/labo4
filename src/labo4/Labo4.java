2bebc45e784247e3712d2259b390d41bd4b585ff
unknown <gb791339@C32476.ens.uqam.ca>
2014-10-01 10:14:14 -0400

    ajout hello world

---
+++ src/labo4/Labo4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package labo4;
+
+/**
+ *
+ * @author gb791339
+ */
+public class Labo4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
