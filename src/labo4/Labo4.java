89562254ace15ff781bfe7b8d146abde5dafdb07
unknown <gb791339@C32476.ens.uqam.ca>
2014-10-01 10:25:49 -0400

    ajout hello world

--- src/labo4/Labo4.java
+++ src/labo4/Labo4.java
@@ -16,6 +16,8 @@ public class Labo4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello World");
+          System.out.println("Hello World");
         // TODO code application logic here
     }
     
